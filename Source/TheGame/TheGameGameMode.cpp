e35eda7ab83aeeea7eb7f838bea5873f27b27fdd
parkjongmin <park6978@naver.com>
2022-04-10 16:10:43 +0900

    first room

---
+++ Source/TheGame/TheGameGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheGameGameMode.h"
+#include "TheGameHUD.h"
+#include "TheGameCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATheGameGameMode::ATheGameGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ATheGameHUD::StaticClass();
+}
